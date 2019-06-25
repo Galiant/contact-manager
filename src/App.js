e7527e09f0ac13a3ad1e0e2a285e589b5d073643
Galiant <antonijodev@gmail.com>
2019-06-25 21:07:44 +0100

    Adding bootstrap to project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import Contact from './components/Contact';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
@@ -8,16 +9,18 @@ class App extends Component {
     return (
       <div className="App">
         <Header branding="Contact Manager" />
-        <Contact
-          name="John Doe"
-          email="johndoe@gmail.com"
-          phone="555-555-5555"
-        />
-        <Contact
-          name="Karen White"
-          email="karenw@gmail.com"
-          phone="444-444-44444"
-        />
+        <div className="container">
+          <Contact
+            name="John Doe"
+            email="johndoe@gmail.com"
+            phone="555-555-5555"
+          />
+          <Contact
+            name="Karen White"
+            email="karenw@gmail.com"
+            phone="444-444-44444"
+          />
+        </div>
       </div>
     );
   }
