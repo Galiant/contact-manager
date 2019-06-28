903f65cd46a0e76b07cad10bd565b846dd524967
Galiant <antonijodev@gmail.com>
2019-06-28 22:19:04 +0100

    Changing state from another component - Delete contact functionality

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSortDown } from '@fortawesome/free-solid-svg-icons';
+import { faSortDown, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 class Contact extends Component {
   state = {
     showContactInfo: false
   };
 
-  onShowClick() {
-    console.log(this.state);
-  }
+  onDeleteClick = () => {
+    this.props.deleteClickHandler();
+  };
+
   render() {
     const { name, email, phone } = this.props.contact;
     const { showContactInfo } = this.state;
@@ -20,12 +21,18 @@ class Contact extends Component {
         <h4>
           {name}
           <FontAwesomeIcon
+            icon={faSortDown}
+            style={{ cursor: 'pointer' }}
             onClick={() =>
               this.setState({
                 showContactInfo: !this.state.showContactInfo
               })
             }
-            icon={faSortDown}
+          />
+          <FontAwesomeIcon
+            icon={faTimes}
+            style={{ cursor: 'pointer', float: 'right', color: 'red' }}
+            onClick={this.onDeleteClick}
           />
         </h4>
         {showContactInfo ? (
@@ -40,7 +47,8 @@ class Contact extends Component {
 }
 
 Contact.propTypes = {
-  contact: PropTypes.object.isRequired
+  contact: PropTypes.object.isRequired,
+  deleteClickHandler: PropTypes.func.isRequired
 };
 
 export default Contact;
